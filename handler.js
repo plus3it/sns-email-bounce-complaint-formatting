57881b08a026b7e1fd8734b656cfd7a2bb84cc3d
moskey71 <29447475+moskey71@users.noreply.github.com>
2018-01-24 10:56:56 -0500

    Fixed error handling, added variable for environment name

--- handler.js
+++ handler.js
@@ -5,18 +5,17 @@ var aws = require('aws-sdk');
 var ses = new aws.SES({
   region: 'us-east-1'
 });
-var toaddress = process.env.toaddress
-var fromaddress = process.env.fromaddress
-
+var toaddress = process.env.toaddress;
+var fromaddress = process.env.fromaddress;
 
 module.exports.handler = (event, context, callback) => {
   //console.log('Received event:', JSON.stringify(event, null, 2));
   const message = JSON.parse(event.Records[0].Sns.Message);
   switch (message.notificationType) {
-    case "Bounce":
+    case 'Bounce':
       handleBounce(message);
       break;
-    case "Complaint":
+    case 'Complaint':
       handleComplaint(message);
       break;
     default:
@@ -69,23 +68,14 @@ function email(id, payload, status) {
         }
       },
       Subject: {
-        Data: "Email " + payload.notificationType + " in Testlab"
+        Data: "Email " + payload.notificationType + " in " + envname
       }
     },
     Source: fromaddress
   };
   console.log('===SENDING EMAIL===');
-  const email = ses.sendEmail(eParams, function(err, data) {
-    if (err) console.log(err);
-    else {
-      console.log("===EMAIL SENT===");
-      console.log(data);
-
-
-      console.log("EMAIL CODE END");
-      console.log('EMAIL: ', email);
-      context.succeed(event);
-
-    }
+  ses.sendEmail(eParams, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data); // successful response
   });
 }
